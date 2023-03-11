c9d3d5145508ee5957dc254a1c976ff082e6458b
Semetey <boronbaevsemetey@gmail.com>
2023-03-11 15:43:51 +0600

    feat: project todo list

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import PostContainer from "./components/Data";
+
+function App() {
+  return (
+    <div className="App">
+      <PostContainer />
+    </div>
+  );
+}
+
+export default App;
